fc55090d966763fd0af7495a987ae60df290ff29
danial <ba.fpsp@gmail.com>
2025-08-24 22:26:30 +0330

    fix some bugs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 import Image from "next/image";
 import './globals.css'
+import Posts from "./posts/list/page";
 
 export default function Home() {
   return (
     <div>
-
+      <Posts />
     </div>
   );
 }
